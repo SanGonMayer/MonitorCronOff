1c7fa3f18aab79bc3696f8095151dee681c29466
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2025-04-08 17:06:52 -0300

    agrego el runtime

--- app/api/connect/route.ts
+++ app/api/connect/route.ts
@@ -1,4 +1,5 @@
 export const dynamic = "force-dynamic";
+export const runtime = 'nodejs';
 export const revalidate = 0;
 import { NextResponse } from "next/server"
 
--- app/api/hosts/route.ts
+++ app/api/hosts/route.ts
@@ -1,3 +1,4 @@
+export const runtime = 'nodejs';
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
 import { NextResponse } from "next/server";
