767ba5a98d14b596f07469bb15a644cbefd73f26
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2025-04-09 12:16:08 -0300

    manejo errores

--- initDatabase.ts
+++ initDatabase.ts
@@ -1,6 +1,14 @@
 import dotenv from "dotenv";
-dotenv.config();  
+dotenv.config();
 
 import { initDatabase } from "./lib/db.js";
 
-initDatabase();
+(async () => {
+  try {
+    await initDatabase();
+    console.log("Script finalizado exitosamente.");
+  } catch (error) {
+    console.error("Ocurrió un error en la inicialización:", error);
+    process.exit(1);
+  }
+})();
