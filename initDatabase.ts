775b58689ab85763ee88058cff88a8d1e2c9e6df
SanGonMayer <sgonzalezmayer@frba.utn.edu.ar>
2025-04-09 12:59:46 -0300

    agrego un log

--- initDatabase.ts
+++ initDatabase.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: ".env" }); // Esto carga el archivo .env que está en la raíz
 
 import { initDatabase } from "./lib/db.js";
 
 (async () => {
   try {
+    console.log("DATABASE_URL:", process.env.DATABASE_URL);
     await initDatabase();
     console.log("Script finalizado exitosamente.");
   } catch (error) {
